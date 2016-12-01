8d97036e27361c234ed09a2d1860b6fa47b2bcda
phatfree <jeffbk95@gmail.com>
2016-11-30 17:26:30 -0800

    Company Add done, minor fix to address by ID too.

---
+++ model/db_connections.example.js
@@ -0,0 +1,7 @@
+
+exports.config = {
+    user: '',
+    password: '',
+    host: '',
+    database: ''
+};
--- routes/company_routes.js
+++ routes/company_routes.js
@@ -16,7 +16,7 @@ router.get('/all', function(req, res) {
 
 });
 
-// View the school for the given id
+// View the company for the given id
 router.get('/', function(req, res){
     if(req.query.company_id == null) {
         res.send('company_id is null');
@@ -29,6 +29,53 @@ router.get('/', function(req, res){
            else {
                res.render('company/companyViewById', {'result': result});
            }
+        });
+    }
+});
+
+// Return the add a new company form
+router.get('/add', function(req, res){
+
+            res.render('company/companyAdd');
+    });
+
+// View the company for the given id
+router.get('/insert', function(req, res){
+    // simple validation
+    if(req.query.company_name == null) {
+        res.send('Company Name must be provided.');
+    }
+    else if(req.query.address_id == null) {
+        res.send('An Address must be selected');
+    }
+    else {
+        // passing all the query parameters (req.query) to the insert function instead of each individually
+        company_dal.insert(req.query, function(err,result) {
+            if (err) {
+                res.send(err);
+            }
+            else {
+                //poor practice, but we will handle it differently once we start using Ajax
+                res.redirect(302, '/company/all');
+            }
+        });
+    }
+});
+
+// Delete a company for the given company_id
+router.get('/delete', function(req, res){
+    if(req.query.company_id == null) {
+        res.send('company_id is null');
+    }
+    else {
+        company_dal.delete(req.query.company_id, function(err, result){
+            if(err) {
+                res.send(err);
+            }
+            else {
+                //poor practice, but we will handle it differently once we start using Ajax
+                res.redirect(302, '/company/all');
+            }
         });
     }
 });
