009b2c4d8ab44eec9853e407a87de31dfcf68ba0
phatfree <jeffbk95@gmail.com>
2016-12-10 19:23:15 -0800

    Preliminary updates to resume DAL, School update partially present

--- model/resume_dal.js
+++ model/resume_dal.js
@@ -43,3 +43,89 @@ exports.delete = function(resume_id, callback) {
     });
 
 };
+
+
+
+//NEEDS ADAPTING
+
+//declare the function so it can be used locally
+var resumeSchoolInsert = function(resume_id, schoolIdArray, callback){
+    // NOTE THAT THERE IS ONLY ONE QUESTION MARK IN VALUES ?
+    var query = 'INSERT INTO resume_school (resume_id, school_id) VALUES ?';
+
+    // TO BULK INSERT RECORDS WE CREATE A MULTIDIMENSIONAL ARRAY OF THE VALUES
+    var resumeSchoolData = [];
+    for(var i=0; i < schoolIdArray.length; i++) {
+        resumeSchoolData.push([resume_id, schoolIdArray[i]]);
+    }
+    connection.query(query, [resumeSchoolData], function(err, result){
+        callback(err, result);
+    });
+};
+//export the same function so it can be used by external callers
+module.exports.resumeSchoolInsert = resumeSchoolInsert;
+
+//declare the function so it can be used locally
+var resumeSchoolDeleteAll = function(resume_id, callback){
+    var query = 'DELETE FROM resume_school WHERE resume_id = ?';
+    var queryData = [resume_id];
+
+    connection.query(query, queryData, function(err, result) {
+        callback(err, result);
+    });
+};
+//export the same function so it can be used by external callers
+module.exports.resumeSchoolDeleteAll = resumeSchoolDeleteAll;
+
+exports.update = function(params, callback) {
+    var query = 'UPDATE resume SET resume_name = ? WHERE resume_id = ?';
+
+    var queryData = [params.resume_name, params.resume_id];
+
+    connection.query(query, queryData, function(err, result) {
+        //delete resume_school entries for this resume_school
+        resumeSchoolDeleteAll(params.resume_id, function(err, result){
+
+            if(params.school_id != null) {
+                //insert resume_school ids
+                resumeSchoolInsert(params.resume_id, params.school_id, function(err, result){
+                    callback(err, result);
+                });}
+            else {
+                callback(err, result);
+            }
+        });
+
+    });
+};
+
+
+/*  Stored procedure used in this example
+ DROP PROCEDURE IF EXISTS resume_getinfo;
+
+ DELIMITER //
+ CREATE PROCEDURE resume_getinfo (_resume_id int)
+ BEGIN
+
+ SELECT * FROM resume WHERE resume_id = _resume_id;
+
+ SELECT sc.*, rs.resume_id FROM school sc
+ LEFT JOIN resume_school rs on sc.school_id = sc.school_id AND resume_id = _resume_id
+ ORDER BY sc.school_name;
+
+ END //
+ DELIMITER ;
+
+ # Call the Stored Procedure
+ CALL resume_getinfo (4);
+
+ */
+
+exports.edit = function(resume_id, callback) {
+    var query = 'CALL resume_getinfo(?)';
+    var queryData = [resume_id];
+
+    connection.query(query, queryData, function(err, result) {
+        callback(err, result);
+    });
+};
--- routes/resume_routes.js
+++ routes/resume_routes.js
@@ -38,8 +38,6 @@ router.get('/', function(req, res){
 
 
 
-
-
 // Return the add a new resume form
 router.get('/add', function(req, res){
     // passing all the query parameters (req.query) to the insert function instead of each individually
@@ -74,6 +72,39 @@ router.get('/insert', function(req, res){
             }
         });
     }
+});
+
+router.get('/edit', function(req, res){
+    if(req.query.resume_id == null) {
+        res.send('A resume id is required');
+    }
+    else {
+        resume_dal.edit(req.query.resume_id, function(err, result){
+            console.log(result);
+            res.render('resume/resumeUpdate', {resume: result[0][0], address: result[1]});
+        });
+    }
+
+});
+
+router.get('/edit2', function(req, res){
+    if(req.query.resume_id == null) {
+        res.send('A resume id is required');
+    }
+    else {
+        resume_dal.getById(req.query.resume_id, function(err, resume){
+            address_dal.getAll(function(err, address) {
+                res.render('resume/resumeUpdate', {resume: resume[0], address: address});
+            });
+        });
+    }
+
+});
+
+router.get('/update', function(req, res) {
+    resume_dal.update(req.query, function(err, result){
+        res.redirect(302, '/resume/all');
+    });
 });
 
 // Delete a school for the given school_id
